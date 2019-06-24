7e8d68b2efb79fae201a10df5754de632eae9f0d
Vipin Varghese <vipin.varghese@intel.com>
2019-06-24 22:45:45 +0530

    src: add dpdk-include
    Signed-off-by: Vipin Varghese <vipinpv85@gmail.com>

---
+++ suricata-4.1.4/src/dpdk-include-common.h
@@ -0,0 +1,39 @@
+#ifndef __DPDK_INCLUDE_COMMON__
+#define __DPDK_INCLUDE_COMMON__
+
+#include <sys/types.h>
+#include <sys/queue.h>
+
+#include <rte_common.h>
+#include <rte_byteorder.h>
+#include <rte_log.h>
+#include <rte_memory.h>
+#include <rte_memcpy.h>
+#include <rte_memzone.h>
+#include <rte_eal.h>
+#include <rte_per_lcore.h>
+#include <rte_launch.h>
+#include <rte_atomic.h>
+#include <rte_cycles.h>
+#include <rte_prefetch.h>
+#include <rte_lcore.h>
+#include <rte_per_lcore.h>
+#include <rte_branch_prediction.h>
+#include <rte_interrupts.h>
+#include <rte_pci.h>
+#include <rte_random.h>
+#include <rte_debug.h>
+#include <rte_mempool.h>
+#include <rte_mbuf.h>
+#include <rte_ether.h>
+#include <rte_ethdev.h>
+#include <rte_ip.h>
+#include <rte_tcp.h>
+#include <rte_udp.h>
+#include <rte_string_fns.h>
+#include <rte_acl.h>
+#include <rte_version.h>
+#include <rte_tailq.h>
+#include <rte_cfgfile.h>
+
+#endif /*__DPDK_INCLUDE_COMMON__*/
